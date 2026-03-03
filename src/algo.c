95c64685837d103769dd4782f0534a1ee598a2ca
Quentin Hoarau <quentin.hoarau@epitech.eu>
2026-03-03 16:53:35 +0100

    calcul of distance of each room

--- src/algo.c
+++ src/algo.c
@@ -53,18 +53,35 @@ room_t *remove_from_queue(queue_t *queue)
     return room;
 }
 
+void go_in_tunnels(tunnel_t *current_tunnel, room_t *current_room, queue_t *queue) 
+{
+    while (current_tunnel != NULL) {
+        if (current_tunnel->room_ptr->distance == -1) {
+            current_tunnel->room_ptr->distance = current_room->distance + 1;
+            add_to_queue(queue, current_tunnel->room_ptr);
+        }
+        current_tunnel = current_tunnel->next;
+    }
+}
+
 int calcul_distances(room_t *head, room_t *end)
 {
-    room_t *current = head;
-    queue_t *queue = malloc(sizeof(queue_t));
+    room_t *current_room = NULL;
+    tunnel_t *current_tunnel = NULL;
+    queue_t *queue = init_queue();
 
-    if (!head)
+    if (!head || !end || !queue)
         return 84;
-    while (current) {
-        current->distance = -1;
-        current = current->next;
+    for (room_t *tmp = head; tmp != NULL; tmp = tmp->next) {
+        tmp->distance = -1;
     }
     end->distance = 0;
-
+    add_to_queue(queue, end);
+    while (queue->front != NULL) {
+        current_room = remove_from_queue(queue);
+        current_tunnel = current_room->tunnels;
+        go_in_tunnels(current_tunnel, current_room, queue);
+    }
+    free(queue);
     return 0;
 }
